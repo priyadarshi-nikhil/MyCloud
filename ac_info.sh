436e7317e0a294cbad3305e57c63491239a4d30d
root <nikhilpriyadarshi1@gmail.com>
2019-10-17 10:40:59 +0530

    A/c info updated.

--- ac_info.sh
+++ ac_info.sh
@@ -12,4 +12,24 @@ echo "You have a $t subscription."
 lxc-attach -n cloud-lxc -- chage --list $name | grep Account| awk '{print $1,$2,$3,$4,$5,$6}'
 echo "Storage limit :$s GB"
 echo
+
+
+disk=$(lxc-attach -n cloud-lxc -- du -h /home/$name)
+set -- $disk
+echo " Your current disk usage is: $1"
+
+
+bites=$(lxc-attach -n cloud-lxc -- du -b /home/$name)
+set -- $bites
+leftBite=$((s*1073741824))
+a=$((leftBite-$1))
+b=`echo "scale=4;$a/1073741824" | bc -l`
+echo "You have $b GB space left." 
+
+
+x=$(lxc-attach -n cloud-lxc -- chage --list $name | grep Account| awk '{print $4,$5,$6}')
+ 
+daysLeft=$((($(date +%s --date "$x")-$(date +%s))/(3600*24)))
+
+echo " $daysLeft left for your account to expire."
 ./landing.sh
